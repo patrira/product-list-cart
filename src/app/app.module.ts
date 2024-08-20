45bb46f3562f243660cd1953b390fc1bdb200ba5
patrira <iradukundapatrick19@gmail.com>
2024-08-20 16:30:04 +0200

    initial commit and project structuring

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,26 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CartComponent } from './components/cart/cart.component';
+import { HomeComponent } from './components/home/home.component';
+import { DesertComponent } from './components/desert/desert.component';
+import { ConfirmOrderComponent } from './components/confirm-order/confirm-order.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CartComponent,
+    HomeComponent,
+    DesertComponent,
+    ConfirmOrderComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    
+    
   ],
   providers: [
     provideClientHydration()
---
+++ src/app/components/desert/desert.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-desert',
+  templateUrl: './desert.component.html',
+  styleUrl: './desert.component.css'
+})
+export class DesertComponent {
+
+}
