d2366d9243afc3a59064dad6cbee61bc41b9d806
patrira <iradukundapatrick19@gmail.com>
2024-08-21 11:11:29 +0200

    fixing styles

--- src/app/components/confirm-order/confirm-order.component.ts
+++ src/app/components/confirm-order/confirm-order.component.ts
@@ -1,16 +1,6 @@
-import {
-  Component,
-  EventEmitter,
-  Input,
-  
-  OnInit,
-  Output,
-  
-} from '@angular/core';
+import {Component,EventEmitter,Input,OnInit,Output,} from '@angular/core';
 import { Subscription } from 'rxjs';
 import { Changes } from '../../models/changes.model';
-
-import { CommonModule } from '@angular/common';
 import { FormatPrice } from '../../priceFormat';
 
 @Component({
